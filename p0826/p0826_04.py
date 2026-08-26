faa937d194273bedf61e540f15a220082a5d347e
이소윤 <leesso0606@gmail.com>
2026-08-26 12:24:35 +0900

    한줄 추가

--- p0826/p0826_04.py
+++ p0826/p0826_04.py
@@ -4,9 +4,11 @@
 
 total1=1000
 send=int(input("송금금액을 입력하세요.")) #100
-total2= toal1+send #1100
+total2= total1+send #1100
 
 print("잔액:{}".format(total1))
 print("송금금액:{}".format(send))
 print("총금액:{}".format(total1+send)) # 내가 한것
+# 한줄로 넣는거면 print("잔액:{}, 송금금액:{}, 총금액:::{}".format(total1, send, total2))
+
 
