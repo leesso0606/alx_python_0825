61f29f0d9d1d0eac2ac8fa434de13abaccede194
이소윤 <leesso0606@gmail.com>
2026-08-26 13:20:26 +0900

    실험

--- p0826/p0826_04.py
+++ p0826/p0826_04.py
@@ -1,14 +1,16 @@
+print(bin(5))
+
 # 잔액 : 1000
 # 송금금액 : 100
 # 총금액을 출력하시오
 
-total1=1000
-send=int(input("송금금액을 입력하세요.")) #100
-total2= total1+send #1100
+# total1=1000
+# send=int(input("송금금액을 입력하세요.")) #100
+# total2= total1+send #1100
 
-print("잔액:{}".format(total1))
-print("송금금액:{}".format(send))
-print("총금액:{}".format(total1+send)) # 내가 한것
+# print("잔액:{}".format(total1))
+# print("송금금액:{}".format(send))
+# print("총금액:{}".format(total1+send)) # 내가 한것
 # 한줄로 넣는거면 print("잔액:{}, 송금금액:{}, 총금액:::{}".format(total1, send, total2))
 
 
