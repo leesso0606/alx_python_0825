c2916a7098fb31ffa1f0c76354168e3d985d1f2e
이소윤 <leesso0606@gmail.com>
2026-08-27 17:13:05 +0900

    삭제,정렬

--- p0827/p0827_07.py
+++ p0827/p0827_07.py
@@ -1,15 +1,135 @@
-fruit=["사과","수박","딸기","참외","복숭아"] #숫자 0부터 시작
-print(fruit[2]) # 딸기
-print(fruit[1:4]) # 1,2,3번까지 출력, 뒷 숫자 전까지 출력/ 수박, 딸기,참외
-print(fruit[2:]) # 2번부터 끝까지 출력/ 딸기, 참외, 복숭아
-print(fruit[:3]) # 처음부터 3번 앞에까지 출력 / 사과, 수박, 딸기
-print(fruit[:]) # 모두 출력/ 사과, 수박, 딸기,참외,복숭아
-print(fruit[::2]) # 처음부터 끝까지 2간격 마다 출력 / 사과, 딸기, 복숭아
-# [:]는 슬라이싱이라고 말함.
-arr=[1,2,3,4,5,6,7,8,9]
-print(arr[::2]) # 홀수만 나옴
-print(arr[1::2]) # 짝수만 나옴
-print(arr[0],arr[2]) # 각각 지정해 줘야 출력됨 / 1,3
+# 정렬 순차정렬: 리스트명.sort(), 역순정렬: 리스트명.sort(reverse=True)
+arr=[1,5,8,3,2] #리스트
+arr.sort()
+print(arr) #[1, 2, 3, 5, 8]
+arr.sort(reverse=TimeoutError)
+print(arr) # [8, 5, 3, 2, 1]
+
+
+
+
+# # 리스트 삭제 - del, pop(), remove, clear(모두삭제)
+# arr=[1,2,3,4,5,True,"안녕"]
+# print(arr) #[1, 2, 3, 4, 5, True, '안녕']
+# # 리스트명.pop()
+# arr.pop(2) # 2번주소 삭제
+# print(arr) #[1, 2, 4, 5, True, '안녕']
+
+# #del 리스트명 []
+# del arr[0] # 0번째 삭제 / del 리스트명[]
+# print(arr) #[2, 4, 5, True, '안녕']
+
+# # 리스트명.remove()
+# arr.remove("안녕")
+# print(arr) #[2, 4, 5, True]
+# # 리스트명.clear() -> 전부삭제
+# arr.clear()
+# print(arr) #[]
+
+
+
+
+
+
+
+
+
+
+
+# # 리스트 추가
+# a=[1,2,3]
+# b=[4,5,6]
+# #원본에 영향이 없음
+# print(a+b)
+# print(a)
+# # 원본에 영향이 있음 
+# a.extend(b)
+# print(a) # [1, 2, 3, 4, 5, 6] a원본은 값을 직접 변경해서 추가해줌.
+# print(b)
+
+
+
+
+
+
+# # 리스트 추가 :append, insert
+# # append는 뒤에추가
+# arr=[1,2]
+# arr.append(3)
+# arr.append(9)
+# arr.append(5)
+# print(arr)
+# # [1, 2, 3, 9, 5]
+# # insert : 원하는 위치에 추가 / 어지간하면 사용안함. 데이터가 많을 수록 오래걸림.
+# arr.insert (1,20)
+# print(arr) #[1, 20, 2, 3, 9, 5]
+
+
+
+
+
+# arr1=[1,2,3]
+# arr2=[4,5]
+# arr3=arr1+arr2 # 리스트+리스트=리스트 /리스트끼리 합쳐짐
+# print(arr1+arr2)
+# print(arr3)
+
+# arr4=arr1*3
+# print(arr4) # 반복됨.
+
+# aaa=[0,0,0,0,0,0,0,0,0,0]
+# aaa2=[0]*10 # 둘 같은말
+# print(aaa)
+# print(aaa2)
+# print(aaa==aaa2)
+
+
+
+
+
+
+
+# arr=[[1,2,3],[4,5,6],[7,8,9]]
+# arr=[
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ] # 둘 동일한 말. 리스트는 \는 안 넣어도됨.
+# print(arr[1]) # [4,5,6]
+# print(arr[1][1]) # 5
+
+
+
+# # 문자열-리스트형대로 저장가능.
+# name="안녕하세요반갑습니다"
+# print(name) #안녕하세요반갑습니다
+# print(name[1]) #녕
+# print(name[6]) #갑
+# print(name[5:8]) #반갑습
+# print(name[::2]) #안하요갑니
+# if "하" in name:
+#     print("있습니다.")
+# else:
+#     print("없습니다.") #있습니다.
+
+
+
+
+
+# fruit=["사과","수박","딸기","참외","복숭아"] #숫자 0부터 시작
+# print(fruit[2]) # 딸기
+# print(fruit[1:4]) # 1,2,3번까지 출력, 뒷 숫자 전까지 출력/ 수박, 딸기,참외
+# print(fruit[2:]) # 2번부터 끝까지 출력/ 딸기, 참외, 복숭아
+# print(fruit[:3]) # 처음부터 3번 앞에까지 출력 / 사과, 수박, 딸기
+# print(fruit[:]) # 모두 출력/ 사과, 수박, 딸기,참외,복숭아
+# print(fruit[::2]) # 처음부터 끝까지 2간격 마다 출력 / 사과, 딸기, 복숭아
+# # 슬라이싱이라고 말함. [시작:끝:간격] 간격은 없어도 상관없음.
+# arr=[1,2,3,4,5,6,7,8,9]
+# print(arr[::2]) # 홀수만 나옴
+# print(arr[1::2]) # 짝수만 나옴
+# print(arr[:-1]) #뒤부터는 -1부터 시작, 마지막 제외
+# print(arr[::-1]) #거꾸로 출력, 리스트 역순정렬
+# print(arr[0],arr[2]) # 각각 지정해 줘야 출력됨 / 1,3
 
 
 
